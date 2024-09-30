281fd1a7d1630f6af247a14e3ddaf26ca537476c
Dylan Carver <dylancarver14@gmail.com>
2024-09-30 13:21:44 -0500

    Create new wager page

---
+++ client/src/components/CreateWager.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateWager = () => {
+
+  return (
+    <div>
+      <h2>Welcome to the Create Wager Page (Protected)</h2>
+    </div>
+  );
+};
+
+export default CreateWager;
