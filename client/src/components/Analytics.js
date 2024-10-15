4f01203ad89548a5a0eee3d4afabb85eb180a2c4
Dylan Carver <dylancarver14@gmail.com>
2024-10-14 22:32:05 -0500

    Display season data tree in a new analytics component

---
+++ client/src/components/Analytics.js
@@ -0,0 +1,12 @@
+import SeasonDataTree from "./SeasonDataTree";
+
+const Analytics = () => {
+  return (
+    <div>
+      <h2>Welcome to the Analytics Page</h2>
+      <SeasonDataTree />
+    </div>
+  );
+};
+
+export default Analytics;
