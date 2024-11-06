8d541b9608ed7a2539a924e011af4fa7e3ac7e7f
Dylan Carver <dylancarver14@gmail.com>
2024-11-06 14:57:01 -0600

    Import firebase admin to server + Create function to verify a firebase user token for future request auth

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { Server } = require("socket.io");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const firebaseAdmin = require("firebase-admin");
+const firebaseServiceAccountKey = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
 // ************************************************************************************************
 // ************************************************************************************************
@@ -106,6 +108,36 @@ connectToDatabase();
 // *****************************************API ENDPOINTS******************************************
 // ************************************************************************************************
 // ************************************************************************************************
+
+// ************************************************************************************************
+// ************************************************************************************************
+// ********************************************FIREBASE********************************************
+// ************************************************************************************************
+// ************************************************************************************************
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(firebaseServiceAccountKey)
+});
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
+    console.log(decodedToken)
+    req.user = decodedToken; // Attach the decoded token to the request
+    next();
+  } catch (error) {
+    console.error("Error verifying Firebase token:", error);
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
 
 // ************************************************************************************************
 // ************************************************************************************************
