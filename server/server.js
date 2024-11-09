a7d7e04b4b3ded84acc5e496a02764c42148b97d
Dylan Carver <dylancarver14@gmail.com>
2024-11-09 13:19:10 -0600

    Return mongo user id and type when fetching data from server

--- server/server.js
+++ server/server.js
@@ -242,7 +242,11 @@ app.get("/api/users/firebase/:firebaseUserId", verifyFirebaseToken, async (req, 
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    res.status(200).json(user);
+    const response = {
+      id: user._id.toString(),
+      type: user.type,
+    } 
+    res.status(200).json(response);
   } catch (err) {
     res
       .status(500)
