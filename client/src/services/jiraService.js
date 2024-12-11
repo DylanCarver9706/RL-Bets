995af794d5d05715da8028018e8686ab67c78777
Dylan Carver <dylancarver14@gmail.com>
2024-12-11 12:10:35 -0600

    Update the Jira service file so that there is a dynamic function that will create a new Jira issue with specified parameters

--- client/src/services/jiraService.js
+++ client/src/services/jiraService.js
@@ -2,18 +2,18 @@ import { getFirebaseIdToken } from "./firebaseService.js";
 
 const BASE_URL = process.env.REACT_APP_BASE_SERVER_URL;
 
-export const createEmailUpdateRequest = async (userName, userEmail) => {
+export const createJiraIssue = async (userName, userEmail, mongoUserId, issueType, summary, description, status) => {
     try {
 
         const idToken = await getFirebaseIdToken();
 
-      const response = await fetch(`${BASE_URL}/api/jira/email-update-request`, {
+      const response = await fetch(`${BASE_URL}/api/jira/create-issue`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${idToken}`, // Include the token in the headers
         },
-        body: JSON.stringify({ userName, userEmail }),
+        body: JSON.stringify({ userName, userEmail, mongoUserId, issueType, summary, description, status }),
       });
   
       if (!response.ok) {
