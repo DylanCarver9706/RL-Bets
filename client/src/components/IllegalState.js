8c2631f8f3cdb31189681b6eaff471cc14b010fb
Dylan Carver <dylancarver14@gmail.com>
2024-12-23 18:13:31 -0600

    Create IllegalState component to direct users to when their current location is in a state that does not allow sports gambling

---
+++ client/src/components/IllegalState.js
@@ -0,0 +1,10 @@
+const IllegalState = () => {
+
+    return (
+      <div >
+        <h1>Due to state law, you are not allowed to use this app from your location.</h1>
+      </div>
+    );
+  };
+  
+  export default IllegalState;
