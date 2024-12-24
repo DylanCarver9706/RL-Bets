e409ecb382b66d7f48ad310ffb373660adc8d007
Dylan Carver <dylancarver14@gmail.com>
2024-12-23 18:14:26 -0600

    Create the LocationPermissionRequired component to direct users to when they do not enable their location permission within the browser settings or ignore the prompt

---
+++ client/src/components/LocationPermissionRequired.js
@@ -0,0 +1,11 @@
+const LocationPermissionRequired = () => {
+
+    return (
+      <div >
+        <h1>Due to state law, you are not allowed to use this app without disclosing your location.</h1>
+        <h2>Check your browser's permission settings to allow this site to access your location</h2>
+      </div>
+    );
+  };
+  
+  export default LocationPermissionRequired;
