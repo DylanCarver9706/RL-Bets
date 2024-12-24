61329addb8d0558517ef217ac3633c5a63d2993b
Dylan Carver <dylancarver14@gmail.com>
2024-12-24 14:29:43 -0600

    Create an IllegalAge component to send users who are not old enough

---
+++ client/src/components/IllegalAge.js
@@ -0,0 +1,11 @@
+const IllegalAge = () => {
+
+    return (
+      <div >
+        <h1>Due to state law, you are not allowed to use this app while under age.</h1>
+        <h2>Check your state laws to determine the legal age you can perform online sports gambling in this state.</h2>
+      </div>
+    );
+  };
+  
+  export default IllegalAge;
