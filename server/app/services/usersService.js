955eaa2df3906efee7796a27540d5f8dca43c1fa
Dylan Carver <dylancarver14@gmail.com>
2025-01-01 12:18:35 -0600

    Move users logic from server.js to a service module

---
+++ server/app/services/usersService.js
@@ -0,0 +1,98 @@
+// app/services/userService.js
+const { collections } = require("../../database/mongoCollections");
+const { ObjectId } = require("mongodb");
+const {
+  createMongoDocument,
+  updateMongoDocument,
+} = require("../../database/middlewares/mongo");
+
+const getAllUsers = async () => {
+  return await collections.usersCollection.find().toArray();
+};
+
+const getUserById = async (id) => {
+  return await collections.usersCollection.findOne({
+    _id: ObjectId.createFromHexString(id),
+  });
+};
+
+const getUserByFirebaseId = async (firebaseUserId) => {
+  return await collections.usersCollection.findOne({ firebaseUserId });
+};
+
+const createUser = async (userData) => {
+  // Check if a user with the provided email exists and has been deleted
+  const existingUser = await collections.usersCollection.findOne({
+    email: userData.email,
+    accountStatus: "deleted",
+  });
+
+  if (existingUser) {
+    return await updateMongoDocument(
+      collections.usersCollection,
+      existingUser._id,
+      { $set: { ...userData, accountStatus: "active" } },
+      true
+    );
+  }
+
+  return await createMongoDocument(collections.usersCollection, userData, true);
+};
+
+const updateUser = async (id, updateData, io) => {
+  await updateMongoDocument(collections.usersCollection, id, {
+    $set: updateData,
+  });
+
+  const updatedUser = await collections.usersCollection.findOne({
+    _id: ObjectId.createFromHexString(id),
+  });
+
+  // Emit WebSocket updates
+  const allUsers = await getAllUsers();
+  io.emit("updateUser", updatedUser);
+  io.emit("updateUsers", allUsers);
+
+  return updatedUser;
+};
+
+const softDeleteUser = async (id, io) => {
+  await updateMongoDocument(collections.usersCollection, id, {
+    $set: {
+      name: null,
+      firebaseUserId: null,
+      credits: 0.0,
+      earnedCredits: 0.0,
+      idvStatus: "unverified",
+      emailVerificationStatus: "unverified",
+      accountStatus: "deleted",
+      DOB: null,
+    },
+  });
+
+  const updatedUser = await collections.usersCollection.findOne({
+    _id: ObjectId.createFromHexString(id),
+  });
+
+  // Emit WebSocket updates
+  const allUsers = await getAllUsers();
+  io.emit("updateUser", updatedUser);
+  io.emit("updateUsers", allUsers);
+};
+
+const deleteUser = async (id) => {
+  const result = await collections.usersCollection.deleteOne({
+    _id: ObjectId.createFromHexString(id),
+  });
+  if (result.deletedCount === 0) throw new Error("User not found");
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  getUserByFirebaseId,
+  createUser,
+  updateUser,
+  softDeleteUser,
+  deleteUser,
+};
