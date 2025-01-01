c3070f6c87fb7f238de92aeaa3bb25de6428faaa
Dylan Carver <dylancarver14@gmail.com>
2025-01-01 16:37:27 -0600

    Include additional forgotten collections from server.js to the new express app

--- server/database/mongoCollections.js
+++ server/database/mongoCollections.js
@@ -10,6 +10,11 @@ const initializeCollections = async () => {
   collections.betsCollection = db.collection("Bets");
   collections.seasonsCollection = db.collection("Seasons");
   collections.tournamentsCollection = db.collection("Tournaments");
+  collections.seriesCollection = db.collection("Series");
+  collections.matchesCollection = db.collection("Matches");
+  collections.teamsCollection = db.collection("Teams");
+  collections.playersCollection = db.collection("Players");
+  console.log("Collections initialized")
 };
 
 module.exports = { initializeCollections, collections };
