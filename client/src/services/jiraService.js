7ca5c988f5b6464abda7b02e2eb6bb709f599aa2
Dylan Carver <dylancarver14@gmail.com>
2025-01-02 15:16:09 -0600

    Remove firebase verification auth header when calling createJiraIssue function + Throw error on failure

--- client/src/services/jiraService.js
+++ client/src/services/jiraService.js
@@ -1,28 +1,24 @@
-import { getFirebaseIdToken } from "./firebaseService.js";
-
 const BASE_URL = process.env.REACT_APP_BASE_SERVER_URL;
 
 export const createJiraIssue = async (userName, userEmail, mongoUserId, issueType, summary, description, status) => {
     try {
 
-        const idToken = await getFirebaseIdToken();
-
       const response = await fetch(`${BASE_URL}/api/jira/create-issue`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${idToken}`, // Include the token in the headers
         },
         body: JSON.stringify({ userName, userEmail, mongoUserId, issueType, summary, description, status }),
       });
   
       if (!response.ok) {
-        throw new Error("Failed to create Jira email update issue.");
+        throw new Error("Failed to create Jira issue.");
       }
   
       return response;
     } catch (error) {
       console.error("Error:", error.message);
+      throw new Error("Failed to create Jira issue.");
     }
   };
   
