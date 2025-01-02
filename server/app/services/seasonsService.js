4d9e5d3e46ae763e3cf9c6ec112e0d0b88776c27
Dylan Carver <dylancarver14@gmail.com>
2025-01-01 19:48:31 -0600

    Update deprecated mongo ObjectId class call to ObjectId object call in seasonsService

--- server/app/services/seasonsService.js
+++ server/app/services/seasonsService.js
@@ -1,12 +1,15 @@
 const { collections } = require("../../database/mongoCollections");
 const {
-    createMongoDocument,
-    updateMongoDocument,
-  } = require("../../database/middlewares/mongo");
+  createMongoDocument,
+  updateMongoDocument,
+} = require("../../database/middlewares/mongo");
 const { ObjectId } = require("mongodb");
 
 const createSeason = async (seasonData) => {
-  const result = await createMongoDocument(collections.seasonsCollection, seasonData);
+  const result = await createMongoDocument(
+    collections.seasonsCollection,
+    seasonData
+  );
   return result.insertedId;
 };
 
@@ -15,25 +18,43 @@ const getAllSeasons = async () => {
 };
 
 const getSeasonById = async (id) => {
-  return await collections.seasonsCollection.findOne({ _id: new ObjectId(id) });
+  return await collections.seasonsCollection.findOne({
+    _id: ObjectId.createFromHexString(id),
+  });
 };
 
 const updateSeason = async (id, updateData) => {
-  if (updateData.winner) updateData.winner = new ObjectId(updateData.winner);
-  if (updateData.loser) updateData.loser = new ObjectId(updateData.loser);
+  if (updateData.winner)
+    updateData.winner = ObjectId.createFromHexString(updateData.winner);
+  if (updateData.loser)
+    updateData.loser = ObjectId.createFromHexString(updateData.loser);
 
-  await updateMongoDocument(collections.seasonsCollection, id, { $set: updateData });
+  await updateMongoDocument(collections.seasonsCollection, id, {
+    $set: updateData,
+  });
 
   if (updateData.status) {
-    const wagers = await collections.wagersCollection.find({ rlEventReference: id }).toArray();
+    const wagers = await collections.wagersCollection
+      .find({ rlEventReference: id })
+      .toArray();
     for (const wager of wagers) {
-      await updateMongoDocument(collections.wagersCollection, wager._id, { $set: { status: updateData.status } });
+      await updateMongoDocument(collections.wagersCollection, wager._id, {
+        $set: { status: updateData.status },
+      });
     }
   }
 };
 
 const deleteSeason = async (id) => {
-  await collections.seasonsCollection.deleteOne({ _id: new ObjectId(id) });
+  await collections.seasonsCollection.deleteOne({
+    _id: ObjectId.createFromHexString(id),
+  });
 };
 
-module.exports = { createSeason, getAllSeasons, getSeasonById, updateSeason, deleteSeason };
+module.exports = {
+  createSeason,
+  getAllSeasons,
+  getSeasonById,
+  updateSeason,
+  deleteSeason,
+};
