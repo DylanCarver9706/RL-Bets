a0e56a86d6092e942ff326e8af778735eedfe839
Dylan Carver <dylancarver14@gmail.com>
2025-01-01 19:43:19 -0600

    Update import route for firebase helper in jiraRoutes

--- server/app/routes/jiraRoutes.js
+++ server/app/routes/jiraRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { createIssueAndTransitionStatus } = require("../controllers/jiraController");
-const { verifyFirebaseToken } = require("../middlewares/verifyFirebaseToken");
+const { verifyFirebaseToken } = require("../middlewares/firebaseAdmin");
 
 router.post("/create-issue", verifyFirebaseToken, createIssueAndTransitionStatus);
 
