cea6c0216f43aededc57982cd14c2e2a3e6ba6a4
Dylan Carver <dylancarver14@gmail.com>
2025-01-01 19:45:35 -0600

    Update deprecated mongo ObjectId class call to ObjectId object call in betsService

--- server/app/services/betsService.js
+++ server/app/services/betsService.js
@@ -1,35 +1,44 @@
 const { collections } = require("../../database/mongoCollections");
 const {
-    createMongoDocument,
-    updateMongoDocument,
-  } = require("../../database/middlewares/mongo");
+  createMongoDocument,
+  updateMongoDocument,
+} = require("../../database/middlewares/mongo");
 const { ObjectId } = require("mongodb");
 
 const createBet = async (betData) => {
   const { user, credits, agreeBet, rlEventReference, wagerId } = betData;
 
   // Validate user and their credits
-  const userDoc = await collections.usersCollection.findOne({ _id: new ObjectId(user) });
+  const userDoc = await collections.usersCollection.findOne({
+    _id: ObjectId.createFromHexString(user),
+  });
   if (!userDoc) throw new Error("User not found");
   if (userDoc.credits < credits) throw new Error("Insufficient credits");
 
   const newBet = {
-    user: new ObjectId(user),
+    user: ObjectId.createFromHexString(user),
     credits,
     agreeBet,
-    rlEventReference: new ObjectId(rlEventReference),
-    wagerId: new ObjectId(wagerId),
+    rlEventReference: ObjectId.createFromHexString(rlEventReference),
+    wagerId: ObjectId.createFromHexString(wagerId),
   };
 
   const result = await createMongoDocument(collections.betsCollection, newBet);
 
   // Update wager with the new bet
-  await updateMongoDocument(collections.wagersCollection, wagerId, { $push: { bets: result.insertedId } });
+  await updateMongoDocument(collections.wagersCollection, wagerId, {
+    $push: { bets: result.insertedId },
+  });
 
   // Deduct credits from user
-  await updateMongoDocument(collections.usersCollection, user, { $set: { credits: userDoc.credits - credits } });
+  await updateMongoDocument(collections.usersCollection, user, {
+    $set: { credits: userDoc.credits - credits },
+  });
 
-  return { betId: result.insertedId, updatedCredits: userDoc.credits - credits };
+  return {
+    betId: result.insertedId,
+    updatedCredits: userDoc.credits - credits,
+  };
 };
 
 const getAllBets = async () => {
@@ -37,18 +46,26 @@ const getAllBets = async () => {
 };
 
 const getBetById = async (id) => {
-  return await collections.betsCollection.findOne({ _id: new ObjectId(id) });
+  return await collections.betsCollection.findOne({
+    _id: ObjectId.createFromHexString(id),
+  });
 };
 
 const updateBet = async (id, updateData) => {
   if (updateData.rlEventReference) {
-    updateData.rlEventReference = new ObjectId(updateData.rlEventReference);
+    updateData.rlEventReference = ObjectId.createFromHexString(
+      updateData.rlEventReference
+    );
   }
-  await updateMongoDocument(collections.betsCollection, id, { $set: updateData });
+  await updateMongoDocument(collections.betsCollection, id, {
+    $set: updateData,
+  });
 };
 
 const deleteBet = async (id) => {
-  await collections.betsCollection.deleteOne({ _id: new ObjectId(id) });
+  await collections.betsCollection.deleteOne({
+    _id: ObjectId.createFromHexString(id),
+  });
 };
 
 module.exports = { createBet, getAllBets, getBetById, updateBet, deleteBet };
