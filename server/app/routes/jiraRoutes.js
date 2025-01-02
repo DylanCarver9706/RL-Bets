58cd0b7b1634f73d973fdf96b0a2a9e192334a22
Dylan Carver <dylancarver14@gmail.com>
2025-01-02 15:14:36 -0600

    Remove firebase verification for attempts to calling the jira/create-issue endpoint

--- server/app/routes/jiraRoutes.js
+++ server/app/routes/jiraRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { createIssueAndTransitionStatus } = require("../controllers/jiraController");
-const { verifyFirebaseToken } = require("../middlewares/firebaseAdmin");
 
-router.post("/create-issue", verifyFirebaseToken, createIssueAndTransitionStatus);
+router.post("/create-issue", createIssueAndTransitionStatus);
 
 module.exports = router;
