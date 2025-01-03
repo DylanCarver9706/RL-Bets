268089866877cb228e01ef72cef7469fdfebabf6
Dylan Carver <dylancarver14@gmail.com>
2025-01-02 23:41:26 -0600

    Update controller module try/catch error alias from "err" to "error" + user error logger if an error is caught

--- server/app/controllers/wagersController.js
+++ server/app/controllers/wagersController.js
@@ -8,70 +8,58 @@ const {
 } = require("../services/wagersService");
 const { createLog } = require("../services/logsService");
 
-const getAll = async (req, res) => {
+const getAll = async (req, res, logError) => {
   try {
     const wagers = await getAllWagers();
     res.status(200).json(wagers);
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Failed to fetch wagers", details: error.message });
+    logError(error);
   }
 };
 
-const getById = async (req, res) => {
+const getById = async (req, res, logError) => {
   try {
     const wager = await getWagerById(req.params.id);
     res.status(200).json(wager);
   } catch (error) {
-    res
-      .status(404)
-      .json({ error: "Failed to fetch wager", details: error.message });
+    logError(error);
   }
 };
 
-const create = async (req, res) => {
+const create = async (req, res, logError) => {
   try {
     const wagerId = await createWager(req.body);
     await createLog({ ...req.body, type: "Wager Created", wagerId });
     res.status(201).json({ message: "Wager created successfully", wagerId });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Failed to create wager", details: error.message });
+    logError(error);
   }
 };
 
-const update = async (req, res) => {
+const update = async (req, res, logError) => {
   try {
     const updatedWager = await updateWager(req.params.id, req.body);
     res.status(200).json(updatedWager);
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Failed to update wager", details: error.message });
+    logError(error);
   }
 };
 
-const remove = async (req, res) => {
+const remove = async (req, res, logError) => {
   try {
     await deleteWager(req.params.id);
     res.status(200).json({ message: "Wager deleted successfully" });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Failed to delete wager", details: error.message });
+    logError(error);
   }
 };
 
-const removeAll = async (req, res) => {
+const removeAll = async (req, res, logError) => {
   try {
     await deleteAllWagers();
     res.status(200).json({ message: "All wagers deleted successfully" });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Failed to delete wagers", details: error.message });
+    logError(error);
   }
 };
 
