737a170469b6275741ff3d7bf15168fe74c63e3c
Dylan Carver <dylancarver14@gmail.com>
2025-01-02 23:39:51 -0600

    Update service module try/catch error alias from "err" to "error"

--- server/app/services/logsService.js
+++ server/app/services/logsService.js
@@ -9,22 +9,27 @@ const { getSocketIo } = require("../middlewares/socketIO");
 const getAllLogs = async () => {
   try {
     return await collections.logsCollection.find().toArray();
-  } catch (err) {
-    throw new Error("Failed to retrieve logs: " + err.message);
+  } catch (error) {
+    throw new Error("Failed to retrieve logs: " + error.message);
   }
 };
 
 const getLogById = async (logId) => {
   try {
-    return await collections.logsCollection.findOne({ _id: ObjectId.createFromHexString(logId) });
-  } catch (err) {
-    throw new Error("Failed to retrieve log: " + err.message);
+    return await collections.logsCollection.findOne({
+      _id: ObjectId.createFromHexString(logId),
+    });
+  } catch (error) {
+    throw new Error("Failed to retrieve log: " + error.message);
   }
 };
 
 const createLog = async (logData) => {
   try {
-    const result = await createMongoDocument(collections.logsCollection, logData);
+    const result = await createMongoDocument(
+      collections.logsCollection,
+      logData
+    );
 
     // Emit updated logs via WebSocket
     const logs = await getAllLogs();
@@ -32,37 +37,37 @@ const createLog = async (logData) => {
     io.emit("updatedLogs", logs);
 
     return { logId: result.insertedId };
-  } catch (err) {
-    throw new Error("Failed to create log: " + err.message);
+  } catch (error) {
+    throw new Error("Failed to create log: " + error.message);
   }
 };
 
 const updateLogById = async (logId, updateData) => {
   try {
-    await updateMongoDocument(
-      collections.logsCollection,
-      logId,
-      { $set: updateData }
-    );
-  } catch (err) {
-    throw new Error("Failed to update log: " + err.message);
+    await updateMongoDocument(collections.logsCollection, logId, {
+      $set: updateData,
+    });
+  } catch (error) {
+    throw new Error("Failed to update log: " + error.message);
   }
 };
 
 const deleteLogById = async (logId) => {
   try {
-    const result = await collections.logsCollection.deleteOne({ _id: ObjectId.createFromHexString(logId) });
+    const result = await collections.logsCollection.deleteOne({
+      _id: ObjectId.createFromHexString(logId),
+    });
     if (result.deletedCount === 0) throw new Error("Log not found");
-  } catch (err) {
-    throw new Error("Failed to delete log: " + err.message);
+  } catch (error) {
+    throw new Error("Failed to delete log: " + error.message);
   }
 };
 
 const deleteAllLogs = async () => {
   try {
     return await collections.logsCollection.deleteMany({});
-  } catch (err) {
-    throw new Error("Failed to delete all logs: " + err.message);
+  } catch (error) {
+    throw new Error("Failed to delete all logs: " + error.message);
   }
 };
 
