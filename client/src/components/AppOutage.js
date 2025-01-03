4750ad4df6e8247569e7ca7ef91650b1472af2b1
Dylan Carver <dylancarver14@gmail.com>
2025-01-03 00:37:27 -0600

    Create AppOutage component to navigate users to when the server is down

---
+++ client/src/components/AppOutage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppOutage = () => {
+
+  return (
+    <div>
+      <h1>RL Bets is currently down. Please check back later.</h1>
+    </div>
+  );
+};
+
+export default AppOutage;
