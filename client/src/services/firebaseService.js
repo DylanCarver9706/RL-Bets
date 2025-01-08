1b9eadb438278a446123c5c29cf0056ab2468311
Dylan Carver <dylancarver14@gmail.com>
2025-01-08 12:40:44 -0600

    Add service function to log an event when a user goes to a page

--- client/src/services/firebaseService.js
+++ client/src/services/firebaseService.js
@@ -1,4 +1,8 @@
 import { auth } from "../firebaseConfig.js";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "../firebaseConfig";
 
 // Get ID token from the currently authenticated user
 export const getFirebaseIdToken = async () => {
@@ -7,4 +11,12 @@ export const getFirebaseIdToken = async () => {
     throw new Error("User is not authenticated");
   }
   return await currentUser.getIdToken(); // Return a fresh ID token
+};
+
+export const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    logEvent(analytics, "page_view", { page_path: location.pathname });
+  }, [location]);
 };
