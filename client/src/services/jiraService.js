c0c24cc8e1ee62a5c8774c613c497c9a3580275f
Dylan Carver <dylancarver14@gmail.com>
2025-01-07 22:58:34 -0600

    Specify the base url in client jiraService

--- client/src/services/jiraService.js
+++ client/src/services/jiraService.js
@@ -1,9 +1,9 @@
-const BASE_URL = process.env.REACT_APP_BASE_SERVER_URL;
+const BASE_SERVER_URL = process.env.REACT_APP_BASE_SERVER_URL;
 
 export const createJiraIssue = async (userName, userEmail, mongoUserId, issueType, summary, description, status) => {
     try {
 
-      const response = await fetch(`${BASE_URL}/api/jira/create-issue`, {
+      const response = await fetch(`${BASE_SERVER_URL}/api/jira/create-issue`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
