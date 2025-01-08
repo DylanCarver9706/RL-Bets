b5260767b1f878b7b9ebaac63b56460aec148d63
Dylan Carver <dylancarver14@gmail.com>
2025-01-07 22:58:23 -0600

    Specify the base url in client appService

--- client/src/services/appService.js
+++ client/src/services/appService.js
@@ -1,8 +1,8 @@
-const BASE_URL = process.env.REACT_APP_BASE_SERVER_URL;
+const BASE_SERVER_URL = process.env.REACT_APP_BASE_SERVER_URL;
 
 export const checkServerStatus = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/api/server-utils/status`);
+    const response = await fetch(`${BASE_SERVER_URL}/api/server-utils/status`);
     if (!response.ok) {
       return false;
     }
