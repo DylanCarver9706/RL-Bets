0d4e57293786aa6c47ccbc5ac6046df428579ab4
Dylan Carver <dylancarver14@gmail.com>
2025-01-14 14:50:43 -0600

    Change /match_concluded api endpoint request method to POST instead of PUT

--- server/app/routes/matchesRoutes.js
+++ server/app/routes/matchesRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { create, getAll, getById, update, concludeMatch, remove } = require("../controllers/matchesController");
 
+router.post("/match_concluded/:id", concludeMatch);
 router.post("/", create);
 router.get("/", getAll);
 router.get("/:id", getById);
 router.put("/:id", update);
-router.put("/match_concluded/:id", concludeMatch);
 router.delete("/:id", remove);
 
 module.exports = router;
