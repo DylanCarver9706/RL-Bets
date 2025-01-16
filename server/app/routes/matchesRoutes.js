db691dddac1708f7898968f3f42664a1939917b0
Dylan Carver <dylancarver14@gmail.com>
2025-01-15 18:22:17 -0600

    Make a new match route for updating the first blood of a match

--- server/app/routes/matchesRoutes.js
+++ server/app/routes/matchesRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { create, getAll, getById, update, concludeMatch, remove } = require("../controllers/matchesController");
+const { create, getAll, getById, update, concludeMatch, firstBlood, remove } = require("../controllers/matchesController");
 
 router.post("/match_concluded/:id", concludeMatch);
+router.post("/first_blood/:id", firstBlood);
 router.post("/", create);
 router.get("/", getAll);
 router.get("/:id", getById);
