eba26d41f655a48c1e842b55f2a34bb7d6de9b26
Dylan Carver <dylancarver14@gmail.com>
2025-01-15 23:05:29 -0600

    Replace all seasons api endpoint routes with an all tournaments api endpoint route

--- server/app/routes/dataTreeRoutes.js
+++ server/app/routes/dataTreeRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const dataTreesController = require("../controllers/dataTreeController");
 
-router.get("/season/all", dataTreesController.getAllSeasons);
-router.get("/season/:id", dataTreesController.getSeason);
+router.get("/tournament/all", dataTreesController.getAllTournaments);
 router.get("/tournament/:id", dataTreesController.getTournament);
 router.get("/series/:id", dataTreesController.getSeries);
 router.get("/betable", dataTreesController.getBetable);
