a9537a406797483197774bafc79cba5fd37bef1e
Dylan Carver <dylancarver14@gmail.com>
2025-01-16 13:36:27 -0600

    Remove service function to fetch the season data tree for Schedule

--- client/src/services/wagerService.js
+++ client/src/services/wagerService.js
@@ -64,28 +64,6 @@ export const createBet = async (body) => {
   }
 };
 
-// Fetch the data tree for a given season ID
-export const fetchSeasonDataTree = async (seasonId) => {
-  try {
-    const response = await fetch(`${BASE_SERVER_URL}/api/data-trees/season/${seasonId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data tree for season ID: ${seasonId}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.error("Error fetching season data tree:", err.message);
-    throw err;
-  }
-};
-
 // Fetch the teams
 export const fetchTeams = async () => {
   try {
