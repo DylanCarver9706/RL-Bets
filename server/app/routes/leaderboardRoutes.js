c62b12cd03162f8c5ee175e5b4ad8b42e4f2eccf
Dylan Carver <dylancarver14@gmail.com>
2025-01-16 17:45:23 -0600

    Make a new routes module for leaderboards

---
+++ server/app/routes/leaderboardRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const leaderboardController = require("../controllers/leaderboardController");
+
+router.get("/current", leaderboardController.getCurrentLeaderboard);
+router.get("/", leaderboardController.getAllLeaderboards);
+router.get("/:id", leaderboardController.getLeaderboardById);
+router.post("/", leaderboardController.createLeaderboard);
+router.put("/:id", leaderboardController.updateLeaderboard);
+router.delete("/:id", leaderboardController.deleteLeaderboard);
+
+module.exports = router;
