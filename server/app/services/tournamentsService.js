6286a75fcc974f800ff7dd9eb645bb25af08cde7
Dylan Carver <dylancarver14@gmail.com>
2025-01-15 23:07:16 -0600

    Get rid of any season logic from tournament service

--- server/app/services/tournamentsService.js
+++ server/app/services/tournamentsService.js
@@ -6,19 +6,10 @@ const {
 const { ObjectId } = require("mongodb");
 
 const createTournament = async (tournamentData) => {
-  if (!tournamentData.season) {
-    throw new Error("Season ID is required to create a Tournament.");
-  }
 
   const result = await createMongoDocument(
     collections.tournamentsCollection,
     tournamentData
-  );
-
-  await updateMongoDocument(
-    collections.seasonsCollection,
-    tournamentData.season,
-    { $push: { tournaments: result.insertedId } }
   );
 
   return result.insertedId;
@@ -39,8 +30,6 @@ const updateTournament = async (id, updateData) => {
     updateData.winner = ObjectId.createFromHexString(updateData.winner);
   if (updateData.loser)
     updateData.loser = ObjectId.createFromHexString(updateData.loser);
-  if (updateData.season)
-    updateData.season = ObjectId.createFromHexString(updateData.season);
 
   await updateMongoDocument(collections.tournamentsCollection, id, {
     $set: updateData,
@@ -67,10 +56,6 @@ const updateTournament = async (id, updateData) => {
 const deleteTournament = async (id) => {
   const tournament = await getTournamentById(id);
   if (!tournament) throw new Error("Tournament not found");
-
-  await updateMongoDocument(collections.seasonsCollection, tournament.season, {
-    $pull: { tournaments: ObjectId.createFromHexString(id) },
-  });
 
   await collections.tournamentsCollection.deleteOne({
     _id: ObjectId.createFromHexString(id),
