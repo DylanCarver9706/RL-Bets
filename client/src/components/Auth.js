5fa5405ac97449e423bb6566f6034bceed6b9027
Dylan Carver <dylancarver14@gmail.com>
2025-01-15 21:14:43 -0600

    Add auth provider to function call when creating a user in the db in Auth

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -86,6 +86,7 @@ const Auth = () => {
           email,
           firebaseUser.uid,
           referralCode,
+          "email",
         );
 
         // if (providerId === "password") {
@@ -154,6 +155,7 @@ const Auth = () => {
             firebaseUser.email,
             firebaseUser.uid,
             referralCode,
+            "google",
           );
 
           // Log the sign-up event to Firebase Analytics
