419faf35ed8b4da7a7d94b2545f096c941d47b55
Dylan Carver <dylancarver14@gmail.com>
2025-01-17 13:46:49 -0600

    Reorganize Navbar to have more dropdown options + Show the current tournament name in place of the current leaderboard and the current tournament

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,11 +2,21 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useUser } from "../context/UserContext.js";
 import socket from "../services/socket.js";
+import { fetchCurrentTournament } from "../services/leaderboardService.js";
 
 const Navbar = () => {
   const { user, setUser } = useUser();
-  const [hovered, setHovered] = useState(false);
+  const [currentTournament, setCurrentTournament] = useState(null);
+  const [hoveredDropdown, setHoveredDropdown] = useState(null); // Manage hover state for each dropdown
 
+  useEffect(() => {
+    // Fetch the current tournament data
+    const fetchTournament = async () => {
+      setCurrentTournament(await fetchCurrentTournament());
+    }
+    fetchTournament();
+  }, []);
+  
   // Listen for updates from the server
   useEffect(() => {
     socket.on("updateUser", (updateUser) => {
@@ -38,47 +48,76 @@ const Navbar = () => {
         <Link to="/Wagers" style={styles.link}>
           Wagers
         </Link>
-        <Link to="/Profile" style={styles.link}>
-          Profile
-        </Link>
-        <Link to="/Schedule" style={styles.link}>
-          Schedule
-        </Link>
         <div
           style={styles.dropdownContainer}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-          onClick={() => setHovered(false)}
+          onMouseEnter={() => setHoveredDropdown("tournaments")}
+          onMouseLeave={() => setHoveredDropdown(null)}
+        >
+          <span style={styles.link}>Tournaments</span>
+          {hoveredDropdown === "tournaments" && (
+            <div style={styles.dropdownMenu}>
+              {currentTournament && (
+                <Link
+                  to={`/Schedule`}
+                  style={styles.dropdownLink}
+                >
+                  {currentTournament.name}
+                </Link>
+              )}
+            </div>
+          )}
+        </div>
+        <div
+          style={styles.dropdownContainer}
+          onMouseEnter={() => setHoveredDropdown("leaderboards")}
+          onMouseLeave={() => setHoveredDropdown(null)}
         >
           <span style={styles.link}>Leaderboards</span>
-          {hovered && (
+          {hoveredDropdown === "leaderboards" && (
             <div style={styles.dropdownMenu}>
+              {currentTournament && (
+                <Link
+                  to={`/Tournament-Leaderboard`}
+                  style={styles.dropdownLink}
+                >
+                  {currentTournament.name}
+                </Link>
+              )}
               <Link to="/Lifetime-Leaderboard" style={styles.dropdownLink}>
                 Lifetime Leaderboard
-              </Link>
-              <Link to="/Tournament-Leaderboard" style={styles.dropdownLink}>
-                Tournament Leaderboard
               </Link>
             </div>
           )}
         </div>
-        <Link to="/Settings" style={styles.link}>
-          Settings
-        </Link>
         {user.userType === "admin" && (
-          <>
-            <Link to="/Admin" style={styles.link}>
-              Admin
+        <div
+          style={styles.dropdownContainer}
+          onMouseEnter={() => setHoveredDropdown("admin")}
+          onMouseLeave={() => setHoveredDropdown(null)}
+        >
+          <span style={styles.link}>Admin</span>
+          {hoveredDropdown === "admin" && (
+            <div style={styles.dropdownMenu}>
+              <Link to="/Admin" style={styles.dropdownLink}>
+              Home
             </Link>
-            <Link to="/Log" style={styles.link}>
+            <Link to="/Create_Wager" style={styles.dropdownLink}>
+              Create Wager
+            </Link>
+            <Link to="/Log" style={styles.dropdownLink}>
               Logs
             </Link>
-          </>
+            </div>
+          )}
+        </div>
         )}
       </div>
       <div style={styles.navLinks}>
-        <Link to="/Create_Wager" style={styles.link}>
-          Create Wager
+        <Link to="/Profile" style={styles.link}>
+          Profile
+        </Link>
+        <Link to="/Settings" style={styles.link}>
+          Settings
         </Link>
         {user?.credits !== null && (
           <Link to="/Credit-Shop" style={styles.link}>
