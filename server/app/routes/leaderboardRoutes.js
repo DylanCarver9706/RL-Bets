362879e2418c9b68849904e42f685794ef8beafc
Dylan Carver <dylancarver14@gmail.com>
2025-01-16 18:26:18 -0600

    Add lifetime leaderboard api route

--- server/app/routes/leaderboardRoutes.js
+++ server/app/routes/leaderboardRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const leaderboardController = require("../controllers/leaderboardController");
 
 router.get("/current", leaderboardController.getCurrentLeaderboard);
+router.get("/lifetime", leaderboardController.getLifetimeLeaderboard);
 router.get("/", leaderboardController.getAllLeaderboards);
 router.get("/:id", leaderboardController.getLeaderboardById);
 router.post("/", leaderboardController.createLeaderboard);
