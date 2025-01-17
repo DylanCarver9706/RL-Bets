2bc9ba19cc391deb6f7b604fe14aae6c74e1ca74
Dylan Carver <dylancarver14@gmail.com>
2025-01-17 14:06:18 -0600

    Add a data tree route to the api to get the current tournament data tree

--- server/app/routes/dataTreeRoutes.js
+++ server/app/routes/dataTreeRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dataTreesController = require("../controllers/dataTreeController");
 
 router.get("/tournament/all", dataTreesController.getAllTournaments);
+router.get("/tournament/current", dataTreesController.getCurrentDataTree);
 router.get("/tournament/:id", dataTreesController.getTournament);
 router.get("/series/:id", dataTreesController.getSeries);
 router.get("/betable", dataTreesController.getBetable);
