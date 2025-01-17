d84a0c3abcfe754aafbee8514ed45f08376db631
Dylan Carver <dylancarver14@gmail.com>
2025-01-17 13:46:04 -0600

    Add a client service function to fetch the current tournament

--- client/src/services/leaderboardService.js
+++ client/src/services/leaderboardService.js
@@ -21,6 +21,27 @@ export const fetchCurrentLeaderboard = async () => {
   }
 };
 
+export const fetchCurrentTournament = async () => {
+  try {
+    const response = await fetch(`${BASE_SERVER_URL}/api/tournaments/current`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data for current tournaments`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Error fetching current tournaments data:", err.message);
+    throw err;
+  }
+};
+
 export const fetchLifetimeLeaderboard = async () => {
   try {
     const response = await fetch(`${BASE_SERVER_URL}/api/leaderboards/lifetime`, {
