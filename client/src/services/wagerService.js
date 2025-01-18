50e653d0acf0cbf5b1b2c59e6d77851bb7171d11
Dylan Carver <dylancarver14@gmail.com>
2025-01-18 12:51:40 -0600

    Move the capitalize helper function from CreateWager to wagerService to be used elsewhere within the app

--- client/src/services/wagerService.js
+++ client/src/services/wagerService.js
@@ -114,3 +114,8 @@ export const fetchBettableObjects = async () => {
     throw err;
   }
 };
+
+export const capitalize = (str) => {
+  if (!str) return ""; // Handle empty or null strings
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
