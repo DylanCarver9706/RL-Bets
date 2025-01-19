a70a2aae2162c6220bb95c6c99886678abf5a218
Dylan Carver <dylancarver14@gmail.com>
2025-01-19 14:02:16 -0600

    Update nodemailer middleware to support sending attachments

--- server/app/middlewares/nodemailer.js
+++ server/app/middlewares/nodemailer.js
@@ -1,27 +1,25 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, subject, text) => {
+const sendEmail = async (to, subject, text, attachments = []) => {
   try {
-    // Create a transporter using the email and password from .env
     const transporter = nodemailer.createTransport({
-      service: "gmail", // Use Gmail as the service
+      service: "gmail",
       auth: {
-        user: process.env.NODEMAILER_USER_EMAIL, // Your email address from .env
-        pass: process.env.NODEMAILER_EMAIL_PASS, // Your email password from .env
+        user: process.env.NODEMAILER_USER_EMAIL,
+        pass: process.env.NODEMAILER_EMAIL_PASS,
       },
     });
 
-    // Email options
     const mailOptions = {
-      from: process.env.EMAIL_USER, // Sender address
-      to, // Recipient address
-      subject, // Email subject
-      text, // Email content (plain text)
+      from: process.env.NODEMAILER_USER_EMAIL,
+      to,
+      subject,
+      text,
+      attachments, // Attachments array
     };
 
-    // Send the email
     const info = await transporter.sendMail(mailOptions);
-    // console.log("Email sent successfully:", info.response);
+    console.log("Email sent successfully:", info.response);
     return info;
   } catch (error) {
     console.error("Error sending email:", error);
