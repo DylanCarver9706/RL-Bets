e2abfbe3d21a32179c7a4ce126232238c146c9e0
Dylan Carver <dylancarver14@gmail.com>
2025-02-07 11:59:22 -0600

    Add functions to client firebase service to send, fetch, and delete firebase images from storage

--- client/src/services/firebaseService.js
+++ client/src/services/firebaseService.js
@@ -4,6 +4,8 @@ import { useLocation } from "react-router-dom";
 import { logEvent } from "firebase/analytics";
 import { analytics } from "../firebaseConfig";
 
+const BASE_SERVER_URL = process.env.REACT_APP_BASE_SERVER_URL;
+
 // Get ID token from the currently authenticated user
 export const getFirebaseIdToken = async () => {
   const currentUser = auth.currentUser;
@@ -19,4 +21,53 @@ export const usePageTracking = () => {
   useEffect(() => {
     logEvent(analytics, "page_view", { page_path: location.pathname });
   }, [location]);
+};
+
+// firebaseService.js
+export const sendImageToAPI = async (formData) => {
+  try {
+    const response = await fetch(`${BASE_SERVER_URL}/api/firebase/storage/upload`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to upload images to server.");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error uploading images to API:", error);
+    throw error;
+  }
+};
+
+export const fetchIdentityVerificationImages = async () => {
+  try {
+    const response = await fetch(`${BASE_SERVER_URL}/api/firebase/storage/identity-verification-images`);
+    const data = await response.json();
+    return data.images;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+  }
+};
+
+export const deleteUserIdvFiles = async (userId, userName) => {
+  try {
+    const response = await fetch(`${BASE_SERVER_URL}/api/firebase/storage/delete`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userId, userName }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to delete identity verification files.");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error deleting ID verification files:", error);
+    throw error;
+  }
 };
