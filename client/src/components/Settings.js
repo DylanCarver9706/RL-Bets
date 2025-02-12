930c9148cc01892dc033690d8fa8e02570d2d5b1
Dylan Carver <dylancarver14@gmail.com>
2025-02-11 22:50:27 -0600

    Refactor Settings for readability + Add button to get to /Instructions

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -4,36 +4,23 @@ import { useNavigate } from "react-router-dom";
 const Settings = () => {
   const navigate = useNavigate();
 
-  const handleReportProblem = () => {
-    navigate("/Bug-Form");
-  };
-
-  const handleEnhancementRequest = () => {
-    navigate("/Feature-Form");
-  };
-
-  const handleFeedback = () => {
-    navigate("/Feedback-Form");
-  };
-
-  const handleCredits = () => {
-    navigate("/Credits");
-  };
-
   return (
     <div>
       <h2>Settings</h2>
-      <button onClick={handleReportProblem} style={{ margin: "10px" }}>
+      <button onClick={() => {navigate("/Bug-Form")}} style={{ margin: "10px" }}>
         Report a Bug
       </button>
-      <button onClick={handleEnhancementRequest} style={{ margin: "10px" }}>
+      <button onClick={() => {navigate("/Feature-Form")}} style={{ margin: "10px" }}>
         Suggest a Feature
       </button>
-      <button onClick={handleFeedback} style={{ margin: "10px" }}>
+      <button onClick={() => {navigate("/Feedback-Form")}} style={{ margin: "10px" }}>
         Give General Feedback
       </button>
-      <button onClick={handleCredits} style={{ margin: "10px" }}>
+      <button onClick={() => {navigate("/Credits")}} style={{ margin: "10px" }}>
         Credits
+      </button>
+      <button onClick={() => {navigate("/Instructions")}} style={{ margin: "10px" }}>
+        Instructions
       </button>
     </div>
   );
