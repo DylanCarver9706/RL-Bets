3acdde4168b2cd85b458ddde03c9bae419ca4d29
Dylan Carver <dylancarver14@gmail.com>
2025-02-11 23:19:51 -0600

    Create About component to tell the story of the creation of RL Bets

---
+++ client/src/components/About.js
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        RL Bets is a platform to place bets on professional Rocket League
+        tournaments. The idea was always there. Rocket League is a sport with a
+        ball, so it is only fair that we should be able to bet on that. The
+        motivation to make this came when I attended RLCS in 2024 in Dallas,
+        Texas and heard a group of people one row behind me keeping track of how
+        many demos a particular team would have in a series. Their bet was
+        either over/under 30 demos. Though I couldn't hear what they were
+        wagering for this, it didn't matter because this bet brought them
+        together and made them look at the game differently. From the triple
+        flips resets to the own goals, there is always something amazing about
+        Rocket League we can be excited about. That is how I view the game, and
+        this is my contribution to the amazing community of Rocket League
+      </p>
+    </div>
+  );
+};
+export default About;
