1161d2b8076d507d2563c524b5215d1f1389706b
Dylan Carver <dylancarver14@gmail.com>
2025-02-11 23:20:22 -0600

    Create Contact component to display the business email for RLBets

---
+++ client/src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <p>For all inquires, business or other, please email RL Bets at {process.env.REACT_APP_BUSINESS_EMAIL}</p>
+      </div>
+    );
+  };
+  export default Contact;
