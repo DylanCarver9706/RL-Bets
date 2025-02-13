b82cbaeb4ad31ebe0b6c4c0f6201556ac516a1c0
Dylan Carver <dylancarver14@gmail.com>
2025-02-12 23:57:18 -0600

    Update server stripeService to use the madeFirstPurchase attribute to either use the regular or discounted price for the checkout session + Update value in user doc if used

--- server/app/services/stripeService.js
+++ server/app/services/stripeService.js
@@ -4,8 +4,9 @@ const { collections } = require("../../database/mongoCollections");
 const { ObjectId } = require("mongodb");
 const { updateMongoDocument, createMongoDocument } = require("../../database/middlewares/mongo");
 const { getSocketIo } = require("../middlewares/socketIO");
+const { getUserById } = require("./usersService");
 
-const createCheckoutSession = async (purchaseItems, mongoUserId, creditsTotal) => {
+const createCheckoutSession = async (purchaseItems, mongoUserId, userMadeFirstPurchase) => {
   try {
     // Fetch product details from the database
     const productIds = purchaseItems.map(item => ObjectId.createFromHexString(item._id));
@@ -13,15 +14,29 @@ const createCheckoutSession = async (purchaseItems, mongoUserId, creditsTotal) =
       .find({ _id: { $in: productIds } })
       .toArray();
 
-    // Create a map for quick lookup
-    const productMap = productsFromDb.reduce((acc, product) => {
-      acc[product._id.toString()] = product.price; // Store price for comparison
-      return acc;
-    }, {});
+    let productPriceMap = null;
+    const userDoc = await getUserById(mongoUserId)
+    if (userMadeFirstPurchase === false && userDoc.madeFirstPurchase === false) {
+      // Create a map for quick lookup
+      productPriceMap = productsFromDb.reduce((acc, product) => {
+        acc[product._id.toString()] = product.firstPurchaseDiscountPrice; // Store price for comparison
+        return acc;
+      }, {});
+    } else {
+      // Create a map for quick lookup
+      productPriceMap = productsFromDb.reduce((acc, product) => {
+        acc[product._id.toString()] = product.price;
+        return acc;
+      }, {});
+    }
+
+
+    let creditsTotal = 0;
 
     // Build line items with verified prices
     const lineItems = purchaseItems.map((item) => {
-      const verifiedPrice = productMap[item._id]; // Get price from DB
+      const verifiedPrice = productPriceMap[item._id]; // Get price from DB
+      creditsTotal += item.credits * item.quantity;
       if (!verifiedPrice) {
         throw new Error(`Product with ID ${item._id} not found in database`);
       }
@@ -43,7 +58,7 @@ const createCheckoutSession = async (purchaseItems, mongoUserId, creditsTotal) =
       mode: "payment",
       success_url: `${process.env.DEV_CLIENT_URL}/Wagers`,
       cancel_url: `${process.env.DEV_CLIENT_URL}/Credit-Shop`,
-      metadata: { mongoUserId, creditsTotal },
+      metadata: { mongoUserId, creditsTotal, userMadeFirstPurchase },
     });
 
     return session;
@@ -60,6 +75,7 @@ const handleWebhookEvent = async (event, io) => {
   if (event.type === "checkout.session.completed") {
     const userId = session.metadata.mongoUserId;
     const creditsPurchased = session.metadata.creditsTotal;
+    const userMadeFirstPurchase = session.metadata.userMadeFirstPurchase;
 
     try {
       const user = await collections.usersCollection.findOne({
@@ -73,10 +89,16 @@ const handleWebhookEvent = async (event, io) => {
 
       const updatedCredits = (user.credits || 0) + parseFloat(creditsPurchased);
 
+      let updateUserObj = { credits: updatedCredits }
+
+      if (userMadeFirstPurchase === "false") {
+        updateUserObj.madeFirstPurchase = true;
+      }
+
       const updatedUser = await updateMongoDocument(
         collections.usersCollection,
         userId,
-        { $set: { credits: updatedCredits } },
+        { $set: updateUserObj },
         true
       );
 
