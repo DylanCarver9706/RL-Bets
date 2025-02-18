84bc5b41d9941da2ab3e3ce9a561054041bf1a44
Dylan Carver <dylancarver14@gmail.com>
2025-02-18 11:51:05 -0600

    Test out Mongoose instead of MongoClient for server deployment

--- server/database/mongoConnection.js
+++ server/database/mongoConnection.js
@@ -1,25 +1,43 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = process.env.MONGO_DB_BASE_URI;
+// const uri = `${process.env.MONGO_DB_BASE_URI.replace(
+//   "MONGO_DB_SECRET_KEY",
+//   process.env.MONGO_DB_SECRET
+// )}/?retryWrites=true&w=majority&appName=${process.env.MONGO_DB_URI_PARAM_APP_NAME}`;
 
-const options = {
-  ssl: true,
-  tls: true,
-  tlsInsecure: false,
-  minPoolSize: 1,
-};
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 
-const client = new MongoClient(uri, options);
+// const connectToDatabase = async () => {
+//   try {
+//     await client.connect();
+//     console.log("Connected to MongoDB");
+//     return client.db("RLBets");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     throw error;
+//   }
+// };
+
+// module.exports = { client, connectToDatabase };
+
+const mongoose = require('mongoose')
 
 const connectToDatabase = async () => {
   try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-    return client.db("RLBets");
+    console.log(process.env.MONGO_DB_URI);
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    // Return the db
+    return mongoose.connection.db;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
   }
 };
 
-module.exports = { client, connectToDatabase };
+module.exports = { connectToDatabase }; 
