bbe4c0cbfccb3216524bd8d8ad66eff0518ac04d
Dylan Carver <dylancarver14@gmail.com>
2025-02-27 22:36:05 -0600

    Update allowed origins for client use

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -4,7 +4,7 @@ import { getFirebaseIdToken } from "./firebaseService";
 let BASE_SERVER_URL;
 if (process.env.REACT_APP_ENV === "production") {
   BASE_SERVER_URL = process.env.REACT_APP_BASE_PROD_SERVER_URL;
-} else {
+} else if (process.env.REACT_APP_ENV === "development") {
   BASE_SERVER_URL = process.env.REACT_APP_BASE_DEV_SERVER_URL;
 }
 
