0b4dcd95327d6d720c05896e345e0e383e642313
Dylan Carver <dylancarver14@gmail.com>
2025-03-03 15:22:54 -0600

    Fix success and cancel url error for Stripe

--- server/app/services/stripeService.js
+++ server/app/services/stripeService.js
@@ -69,12 +69,12 @@ const createCheckoutSession = async (
       mode: "payment",
       success_url: `${
         process.env.ENV === "production"
-          ? process.env.PROD_CLIENT_URL
+          ? process.env.process.env.PROD_CLIENT_URLS.split(",")[1]
           : process.env.DEV_CLIENT_URL
       }/Wagers`,
       cancel_url: `${
         process.env.ENV === "production"
-          ? process.env.PROD_CLIENT_URL
+          ? process.env.PROD_CLIENT_URLS.split(",")[1]
           : process.env.DEV_CLIENT_URL
       }/Credit-Shop`,
       metadata: { mongoUserId, creditsTotal, userMadeFirstPurchase },
